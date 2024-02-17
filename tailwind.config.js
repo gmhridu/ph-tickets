14f6174213cdf7b734fd04a083bf864c9e6f9c62
gmhridu <hasanmahabubhridoy9@gmail.com>
2024-02-17 20:16:38 +0600

    index.html file created

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {},
+  },
+  container: {
+    center: true,
+    padding: "5rem",
+  },
+  daisyui: {
+    themes: [],
+  },
+  plugins: [require("daisyui")],
+};
