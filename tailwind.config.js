1f1baa5b452ea5c03dd5aef0923bd25616c7f2fd
gmhridu <hasanmahabubhridoy9@gmail.com>
2024-02-18 14:38:10 +0600

    select your seat sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,14 @@ module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
     },
     extend: {},
   },
