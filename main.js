51c0083cc324dbc342d88116242cf3b6121e253f
gmhridu <hasanmahabubhridoy9@gmail.com>
2024-02-19 19:01:44 +0600

    updated price section

--- main.js
+++ main.js
@@ -1,50 +1,70 @@
 const buyTickets = document.getElementById("buy-tickets");
+const ticketPrice = document.getElementById("ticket-price");
+const ticketList = document.getElementById("updated-ticket-info");
+const chooseSeats = document.querySelectorAll(".choose-seats");
+const remainingSeatsElement = parseInt(
+  document.getElementById("seats-remaining").innerHTML
+);
+let selectedSeats = 0;
+const totalSeats = chooseSeats.length;
 
+// Scroll to seat selection
 buyTickets.addEventListener("click", function () {
   const seatSelection = document.getElementById("seat-selection");
   seatSelection.scrollIntoView({ behavior: "smooth" });
 });
 
-const chooseSeats = document.querySelectorAll(".choose-seats");
-let count = 0;
-
 for (let seat of chooseSeats) {
   seat.addEventListener("click", function (event) {
-    count += 1;
-
-    const ticketPrice = document.getElementById("ticket-price");
-    const price = ticketPrice.innerText;
-    const priceConverted = parseInt(price);
-    const seats = seat.innerText;
-    const seatClass = document.getElementById("seat-Class");
-    const seatClassText = seatClass.innerText;
-
-    const seatNumberContainer = document.getElementById("seat-number");
-    const seatClassUpdate = document.getElementById("seat-Class");
-    const countPriceContainer = document.getElementById("count-price");
-    const updateSeatInfo = document.getElementById("updated-ticket-info");
-
-    const seatNumberText = document.createElement("p");
-    seatNumberText.innerText = seats;
-
-    const seatClassP = document.createElement("p");
-    seatClassP.innerText = seatClassText;
-
-    const priceUpdate = document.createElement("p");
-    priceUpdate.innerText = priceConverted;
-
-    
-    const clonedSeatNumber = seatNumberText.cloneNode(true);
-    const clonedSeatClass = seatClassP.cloneNode(true);
-    const clonedPriceUpdate = priceUpdate.cloneNode(true);
-
-    seatNumberContainer.appendChild(clonedSeatNumber);
-    seatClassUpdate.appendChild(clonedSeatClass);
-    countPriceContainer.appendChild(clonedPriceUpdate);
-
-    setInnerText("cart-count", count);
+    if (seat.classList.contains("bg-lime-400")) {
+      selectedSeats -= 1;
+    } else {
+      selectedSeats += 1;
+    }
+    setInnerText("cart-count", selectedSeats);
     setBgColor(seat);
+    updateTicketInfo();
+    countPrice();
+    updateRemainingSeats();
   });
+}
+
+function updateTicketInfo() {
+  ticketList.innerHTML = "";
+  for (let seat of chooseSeats) {
+    if (seat.classList.contains("bg-lime-400")) {
+      const wrapper = document.createElement("div");
+      wrapper.classList.add("flex", "justify-around", "items-center");
+      wrapper.classList.add(
+        "font-inter",
+        "text-base",
+        "font-normal",
+        "text-[#03071299]",
+        "text-center"
+      );
+      wrapper.style.padding = "1rem 0";
+      ticketList.appendChild(wrapper);
+      const ticket = document.createElement("p");
+      const ticketClass = document.createElement("p");
+      const ticketPrice = document.createElement("p");
+      ticketClass.innerText = "Economy";
+      ticket.innerText = seat.innerText;
+      ticketPrice.innerText = "550";
+      wrapper.appendChild(ticket);
+      wrapper.appendChild(ticketClass);
+      wrapper.appendChild(ticketPrice);
+    }
+  }
+}
+
+function countPrice() {
+  const totalPrice = 550 * selectedSeats;
+  countPriceContainer.innerText = totalPrice;
+}
+
+function updateRemainingSeats() {
+  const remaining = totalSeats - selectedSeats;
+  remainingSeatsElement.innerText = remaining;
 }
 
 function setInnerText(event, value) {
